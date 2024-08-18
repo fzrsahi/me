cf79d93ba522c0081b678ddb2f36f3cf7100734c
fzrsahi <fazrul.anugrah17@gmail.com>
2024-08-18 22:21:52 +0800

    update configs

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -21,7 +21,7 @@ export const appConfig: appConfigInterface = {
 //Swagger
 export const swaggerConstants = {
   title: 'Fazrul Anugrah Sahi Personal Web',
-  description: 'This is my personal web. but, in backend style, Update 3.0',
+  description: 'This is my personal web. but, in backend style',
   version: '1.0',
   tag: 'Fazrul Anugrah Sahi Endpoints',
   endpoints: '/',
